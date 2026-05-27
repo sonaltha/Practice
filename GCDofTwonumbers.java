094f913a76d6c8b6b90c9662d00ace3943b6dac1
sonaltha <sonalithakur1032@gmail.com>
2026-05-27 20:46:45 +0530

    Add files via upload

---
+++ GCDofTwonumbers.java
@@ -0,0 +1,25 @@
+package telsuko.prac;
+
+import java.util.Scanner;
+
+public class GCDofTwonumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the two numbers: ");
+		int num1=scan.nextInt();
+		int num2=scan.nextInt();
+		
+		int gcd=1;
+		for(int i=1;i<=num1 && i<=num2;++i) {
+			if(num1%i==0 && num2%i==0) {
+				gcd=i;
+			}
+			
+			
+		}System.out.println("GCD of " +num1+ " and " +num2+ " is " +gcd);
+
+	}
+
+}
